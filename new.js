2c801c68901f09776d01039b39c8d2a2b956241f
ArJit0009 <arjitg36@gmaail.com>
2022-03-03 16:45:53 +0530

    newsubmodule created

---
+++ new.js
@@ -0,0 +1,9 @@
+const num1 = 5;
+const num2 = 3;
+
+// add two numbers
+const sum = num1 + num2;
+const sum2 = num1 - num2;
+
+// display the sum
+console.log('The sum of two numbers is ---- ' + num1 + ' and ' + num2 + ' is: ' + sum);
